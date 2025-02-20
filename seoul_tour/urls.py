89fda2331c1971284e7cdfa66eef4af246217d96
Cho YoungHyun <mike135@naver.com>
2025-02-20 17:44:22 +0900

    기본 폴더 및 파일 생성
    각 앱 내 url 생성 및 프로젝트 파일 내 메인 뷰 생성

--- seoul_tour/urls.py
+++ seoul_tour/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from seoul_tour.views import main
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("views.main")),
 ]
